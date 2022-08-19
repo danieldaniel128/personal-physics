e0f26871c15582f077d8ec4a42f83edc31ae8966
daniel kaarur <danielkaarur@gmail.com>
2022-08-19 18:05:57 +0300

    static methods to sum vectors and calculate new vector's angle

--- Custom Physics/Assets/Scripts/Physics/Vector.cs
+++ Custom Physics/Assets/Scripts/Physics/Vector.cs
@@ -26,6 +26,7 @@ public class Vector
     private float AngleCalculator()
     {
         float angle=0;
+        if(X!=0&&Y!=0)
         if (X  > 0 && Y > 0)// + +
             angle = (180 / Mathf.PI) * Mathf.Atan2(Y, X);
         else if (X < 0 && Y < 0)//- -
@@ -36,4 +37,31 @@ public class Vector
             angle = 90-(180 / Mathf.PI) * Mathf.Atan2(X,Y);
         return angle;
     }
+
+    public static float AngleVectorCalculator(Vector vector)
+    {
+        float angle = 0;
+        if (vector.X != 0 && vector.Y != 0)
+            if (vector.X > 0 && vector.Y > 0)// + +
+                angle = (180 / Mathf.PI) * Mathf.Atan2(vector.Y, vector.X);
+            else if (vector.X < 0 && vector.Y < 0)//- -
+                angle = 360 + (180 / Mathf.PI) * Mathf.Atan2(vector.Y, vector.X);
+            else if (vector.X > 0 && vector.Y < 0)//+ -
+                angle = 270 + 180 - (180 / Mathf.PI) * Mathf.Atan2(vector.X, vector.Y);
+            else if (vector.X < 0 && vector.Y > 0)//- +
+                angle = 90 - (180 / Mathf.PI) * Mathf.Atan2(vector.X, vector.Y);
+        return angle;
+    }
+
+    public static Vector SumVectors(Vector a,Vector b) 
+    {
+        Vector c = new Vector(0, 0, 0);
+        c.X= a.X + b.X;
+        c.Y = a.Y + b.Y;
+        c.Z = a.Z + b.Z;
+        c.Angle= AngleVectorCalculator(c);
+        return c;
+    }
+
+
 }
